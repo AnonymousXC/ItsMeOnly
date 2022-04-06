7eecac79866046f42b39161c1da8b043d279b66a
AnonymousXC <cvxanonymous007@gmail.com>
2022-04-05 18:09:46 -1200

    🔥⚡Made Better⚡🔥

--- javascript/aos.js
+++ javascript/aos.js
@@ -15,7 +15,7 @@ AOS.init({
   
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 180, // offset (in px) from the original trigger point
+  offset: 110, // offset (in px) from the original trigger point
   delay: 1, // values from 0 to 3000, with step 50ms
   duration: 500, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
